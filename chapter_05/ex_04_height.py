5db4627f1d5d26a397ed282a32c025d58bbdb3ed
the-twins <marina-budnikova8@rambler.ru>
2025-06-26 11:32:12 +0300

    round inches

--- chapter_05/ex_04_height.py
+++ chapter_05/ex_04_height.py
@@ -4,7 +4,7 @@ print('Enter a height in centimeters: ')
 cm = float(input())
 while(cm > 0):
     inch = cm / CM_IN_INCH
-    print(cm,'cm =', inch // INCH_IN_FOOT, 'feet,', inch % INCH_IN_FOOT, 'inches')
+    print(cm,'cm =', inch // INCH_IN_FOOT, 'feet,', round(inch % INCH_IN_FOOT, 2), 'inches')
     print('Enter a height in centimeters (<= 0 to quit): ')
     cm = float(input())
 print('Bye.')
