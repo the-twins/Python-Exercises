1235f59260c500df32b72bb20c9bfb6d60eceb8a
the-twins <marina-budnikova8@rambler.ru>
2025-08-13 16:20:05 +0300

    Fixed some bugs

--- chapter_07/ex_10_taxes_1988.py
+++ chapter_07/ex_10_taxes_1988.py
@@ -1,3 +1,13 @@
+def print_menu():
+    """Print the menu."""
+    print("Enter the number of your category:")
+    print("1) Single (15% of first $17,850 plus 28% of excess)")
+    print("2) Head of Household (15% of first $23,900 plus 28% of excess)")
+    print("3) Married, Joint (15% of first $29,750 plus 28% of excess)")  
+    print("4) Married, Separate (15% of first $14,875 plus 28% of excess)")
+    print("5) quit")
+    
+    
 RATE1 = 0.15
 RATE2 = 0.28
 TAX_BREAK_1 = 17850.0
@@ -5,52 +15,29 @@ TAX_BREAK_2 = 23900.0
 TAX_BREAK_3 = 29750.0
 TAX_BREAK_4 = 14875.0
 tax = 0
-print("Enter the number of your category:")
-print("1) Single (15% of first $17,850 plus 28% of excess)")
-print("2) Head of Household (15% of first $23,900 plus 28% of excess)")
-print("3) Married, Joint (15% of first $29,750 plus 28% of excess)")  
-print("4) Married, Separate (15% of first $14,875 plus 28% of excess)")
-print("5) quit")
+tax_break = 0
 while True:
+    print_menu()
     numb = int(input())
     if(numb == 1):
-        print("Enter your salary: ")
-        salary = float(input())
-        if(salary <= TAX_BREAK_1):
-            tax = salary * RATE1
-        else:
-            tax = (TAX_BREAK_1 * RATE1) + ((salary - TAX_BREAK_1) * RATE2)
-        print("The gross pay =", salary, "the taxes =", tax, "and the net pay =", salary - tax)
+        tax_break = TAX_BREAK_1
     if(numb == 2):
-        print("Enter your salary: ")
-        salary = float(input())
-        if(salary <= TAX_BREAK_2):
-            tax = salary * RATE1
-        else:
-            tax = (TAX_BREAK_2 * RATE1) + ((salary - TAX_BREAK_2) * RATE2)
-        print("The gross pay =", salary, "the taxes =", tax, "and the net pay =", salary - tax)
+        tax_break = TAX_BREAK_2
     if(numb == 3):
-        print("Enter your salary: ")
-        salary = float(input())
-        if(salary <= TAX_BREAK_3):
-            tax = salary * RATE1
-        else:
-            tax = (TAX_BREAK_3 * RATE1) + ((salary - TAX_BREAK_3) * RATE2)
-        print("The gross pay =", salary, "the taxes =", tax, "and the net pay =", salary - tax)
+        tax_break = TAX_BREAK_3
     if(numb == 4):
-        print("Enter your salary: ")
-        salary = float(input())
-        if(salary <= TAX_BREAK_4):
-            tax = salary * RATE1
-        else:
-            tax = (TAX_BREAK_4 * RATE1) + ((salary - TAX_BREAK_4) * RATE2)
-        print("The gross pay =", salary, "the taxes =", tax, "and the net pay =", salary - tax)
+        tax_break = TAX_BREAK_4
     if(numb == 5):
         break
-    print("Enter the number of your category:")
-    print("1) Single (15% of first $17,850 plus 28% of excess)")
-    print("2) Head of Household (15% of first $23,900 plus 28% of excess)")
-    print("3) Married, Joint (15% of first $29,750 plus 28% of excess)")  
-    print("4) Married, Separate (15% of first $14,875 plus 28% of excess)")
-    print("5) quit")
+    else:
+        print("Input error.")
+        continue
+    print("Enter your salary: ")
+    salary = float(input())
+    if(salary <= tax_break):
+        tax = salary * RATE1
+    else:
+        tax = (tax_break * RATE1) + ((salary - tax_break) * RATE2)
+    print("The gross pay =", salary, "the taxes =", tax, "and the net pay =", salary - tax)
+    print()   
     
