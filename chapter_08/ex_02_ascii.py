2b578c91e6f682c555ce22a61af68f1e036ba259
the-twins <marina-budnikova8@rambler.ru>
2025-08-18 17:14:32 +0300

    Fixed some bugs

--- chapter_08/ex_02_ascii.py
+++ chapter_08/ex_02_ascii.py
@@ -1,17 +1,26 @@
 numb = 0
+let = 64
 stop = False
 while True:
     print("Enter text to be analyzed (& to quit):")
     line = input()
     for i in line:
+        ascii_value = ord(i)
         if(i == '&'):
             stop = True
             break
-        elif(i == '\t'):
-            print(i, '= \\t', end = ' ')
-            numb += 1
+        elif(ascii_value <= 32):
+            for j in range(1, 33):
+                if(j == ascii_value == 9):
+                    print('\\t =', ascii_value, end = ' ')
+                    numb += 1
+                    break
+                elif(j == ascii_value):
+                    print('^', end = '')
+                    print(chr(let + j), '=', ascii_value, end = ' ')
+                    numb += 1 
+                    break                    
         else:
-            ascii_value = ord(i)
             numb += 1
             print(i, '=', ascii_value, end = ' ')
             if(numb % 10 == 0):
@@ -19,6 +28,6 @@ while True:
     if(stop == True):
         break
     numb = 0
-    print('\\n')
+    print('\\n = 10')
     print()
     
