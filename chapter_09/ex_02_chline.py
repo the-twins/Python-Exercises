fa2758ff3beeb503d339b8958e2e5e337ba171a3
the-twins <marina-budnikova8@rambler.ru>
2025-08-28 14:07:49 +0300

    Fixed some bugs

--- chapter_09/ex_02_chline.py
+++ chapter_09/ex_02_chline.py
@@ -1,17 +1,22 @@
-def chline(ch : str, i : int, j : int):
+def chline(ch: str, i: int, j: int):
     """Prints the requested character in columns i through j"""
-    for space in range (1, i):
-        print(" ", end = '')
-    for character in range (i, j + 1):
-        print(ch, end = '')
+    for space in range(1, i):
+        print(' ', end='')
+    for character in range(i, j + 1):
+        print(ch, end='')
 
 
 if __name__ == '__main__':
-    print("Enter one character:")
+    print('Enter one character:')
     ch = input()
-    print("Enter first number:")
+    print('Enter first number:')
     i = int(input())
-    print("Enter second number:")
+    print('Enter second number:')
     j = int(input())
-    chline(ch, i, j)
+    if(i <= 0 or j <= 0):
+        print('Input error. Bye.')
+    elif(i >= j):
+        print('Input error. Bye.')
+    else:
+        chline(ch, i, j)
     
