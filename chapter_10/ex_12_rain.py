cb22949f38f075a4ee8c95e3632db769212cead8
the-twins <marina-budnikova8@rambler.ru>
2025-09-12 17:39:35 +0300

    Fixed some bugs

--- chapter_10/ex_12_rain.py
+++ chapter_10/ex_12_rain.py
@@ -1,28 +1,30 @@
-def sum_rain_in_months(RAIN: list[float]) -> float:
-    """ For each year, sum rainfall for each month and print. Returns total."""
+YEARS = 5
+MONTH = 12
+
+
+def yearly_average(rain: list[list[float]]):
+    """ For each year, sum rainfall for each month and print. Calculates and prints the total."""
     subtot = 0
     total = 0
     year = 0
     print('YEAR    RAINFALL (inches)')
-    for i in range(0, 5):
-        for j in range(0, 12):
+    for i in range(0, YEARS):
+        for j in range(0, MONTH):
             subtot += RAIN[i][j]
         print(2010 + year,'    ', round(subtot, 2))
         total += subtot
         subtot = 0
         year += 1
-    return total
+    print('\nThe yearly average is', round(total / YEARS, 2),'inches.\n')
     
     
-def average_rain(RAIN: list[float], total: float):
+def monthly_average(rain: list[list[float]]):
     """Prints average"""
-    YEARS = 5
     subtot = 0
-    print('\nThe yearly average is', round(total / YEARS, 2),' inches.\n')
     print('MONTHLY AVERAGES:\n')
     print('Jan  Feb  Mar Apr May Jun Jul  Aug  Sep  Oct Nov  Dec')
-    for i in range(0, 12):
-        for j in range(0, 5):
+    for i in range(0, MONTH):
+        for j in range(0, YEARS):
             subtot += RAIN[j][i]
         print(round(subtot / YEARS, 2), end=' ')
         subtot = 0
@@ -35,6 +37,6 @@ if __name__ == '__main__':
         [9.1, 8.5, 6.7, 4.3, 2.1, 0.8, 0.2, 0.2, 1.1, 2.3, 6.1, 8.4],
         [7.2, 9.9, 8.4, 3.3, 1.2, 0.8, 0.4, 0.0, 0.6, 1.7, 4.3, 6.2],
         [7.6, 5.6, 3.8, 2.8, 3.8, 0.2, 0.0, 0.0, 0.0, 1.3, 2.6, 5.2]]
-    total = sum_rain_in_months(RAIN)
-    average_rain(RAIN, total)
+    yearly_average(RAIN)
+    monthly_average(RAIN)
     
--- chapter_10/ex_13_array_3_x_5.py
+++ chapter_10/ex_13_array_3_x_5.py
@@ -1,29 +1,46 @@
-def funct_array(numb: list[float]):
-    """Compute the average of each set of five values, compute the average of all the values,
-    determine the largest value of the 15 values, report the results"""
-    numb_set = 1
+ROWS = 3
+COLS = 5
+
+
+def average_row(n: list[float]):
+    """Computes the average of each set of five values and prints it"""
+    subtot = 0
+    for i in n:
+        subtot += i
+    print('Average of set of five values is', round(subtot / COLS, 2))
+    
+
+def average_all(numb: list[list[float]]) -> float:
+    """Computes the average of all the values and returns it"""
     subtot = 0
     total = 0
-    for i in range(0, 3):
-        for j in range(0, 5):
+    for i in range(0, ROWS):
+        for j in range(0, COLS):
             subtot += numb[i][j]
-        print('Average of', numb_set, 'set of five values is', subtot / 5)
         total += subtot
         subtot = 0
-        numb_set += 1
-    print('Average of all the values is', round(total / 15, 2))
+    return round(total / (ROWS * COLS), 2)
+    
+    
+def largest_value(numb: list[list[float]]) -> float:
+    """Determine the largest value of the 15 values and returns it"""
     max_in_row = [max(row) for row in numb]
     max_numb = max(max_in_row)
-    print('The largest value of the 15 values is', max_numb)
+    return max_numb
     
     
 if __name__ == '__main__':
     numb = []
+    array =[]
     print('Create three sets of five float number each.')
-    for i in range(0, 3):
+    for i in range(0, ROWS):
         numb.append([])
-        for j in range(0, 5):
+        for j in range(0, COLS):
             print('Please enter float number:')
             numb[i].append(float(input()))
     print(numb)
-    funct_array(numb)
+    for n in numb:
+        average_row(n)
+    print('Average of all the values is', average_all(numb))
+    print('The largest value of the 15 values is', largest_value(numb))
+    
