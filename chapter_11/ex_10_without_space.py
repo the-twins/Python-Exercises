85be72482f6030964921a77da719d9fe2c6b269c
the-twins <marina-budnikova8@rambler.ru>
2025-09-18 14:34:49 +0300

    Chapter 11. Exercises 03, 04, 10.

---
+++ chapter_11/ex_10_without_space.py
@@ -0,0 +1,21 @@
+def del_space(some_text: list[str]):
+    """Takes a string as an argument and removes the spaces from the string."""
+    word = ""
+    for i in some_text:
+        if i.isspace() == True:
+            continue
+        else:
+            word += i   
+    print(word)
+    
+    
+if __name__ == '__main__':
+    while True:
+        print('Enter text to remove spaces or empty line to quit:')
+        some_text = input()
+        if(some_text == ""):
+            break
+        del_space(some_text)
+        some_text = ""
+    print('Bye!')
+        
