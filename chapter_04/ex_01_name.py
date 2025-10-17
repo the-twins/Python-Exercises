c42340be548e7e9f5ea64b84aed53070a6243afa
the-twins <marina-budnikova8@rambler.ru>
2025-10-17 15:34:31 +0300

    Chapter 04. Exercises 01, 02, 04, 05.

---
+++ chapter_04/ex_01_name.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    print('Enter your name:')
+    name = input()
+    print('Enter your surname:')
+    surname = input()
+    print('{1}, {0}'.format(name, surname))
+    
