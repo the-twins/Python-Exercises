04d040d51db5c648096dc928e61d557d38b509fd
the-twins <marina-budnikova8@rambler.ru>
2025-10-20 16:31:27 +0300

    Chapter 04. Exercises 06, 08

---
+++ chapter_04/ex_06_name_and_count.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    print('Enter your name:')
+    name = input()
+    print('Enter your surname:')
+    surname = input()
+    n = len(name)
+    s = len(surname)
+    print(f'{name} {surname}')
+    print(f'{0:>{n}} {1:>{s}}'.format(n, s))
+    print(f'{name} {surname}')
+    print(f'{0:<{n}} {1:<{s}}'.format(n, s))
