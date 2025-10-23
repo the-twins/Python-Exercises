521cdb0de84d512cc63ed75544d19d56ffc9b1aa
the-twins <marina-budnikova8@rambler.ru>
2025-10-23 14:44:09 +0300

    Fixed some bugs

--- chapter_13/ex_02_copy.py
+++ chapter_13/ex_02_copy.py
@@ -1,20 +1,15 @@
 import sys
 
 if __name__ == '__main__':
-    print('Enter the file name:')
-    name = input()
     try:
-        with open(name, 'rb') as f:
-            some_text = f.read()
-            count = len(some_text)
-            if(count == 0):
-                print("Can't open", name, "or the file does not contain character")
-            else:
-                with open(sys.argv[1], 'wb') as fc:
-                    fc.write(some_text)
-                    print('Done')
+        if(len(sys.argv) <= 1 or len(sys.argv) > 3):
+            print('Invalid input on command line')
+        else:        
+            with open(sys.argv[1], 'rb') as f:
+                some_data = f.read()
+            with open(sys.argv[2], 'wb') as fc:
+                fc.write(some_data)
+                print('Done')
     except FileNotFoundError:
         print('Error. File not found')
-    except Exception:
-        print('Invalid input on command line')
-        
+    
--- chapter_13/ex_03_copy_with_toupper.py
+++ chapter_13/ex_03_copy_with_toupper.py
@@ -6,15 +6,11 @@ if __name__ == '__main__':
     print('Enter the name of output file:')
     copy_name = input()
     try:
-        with open(name, 'rb') as f:
+        with open(name, 'r') as f:
             some_text = f.read()
-            count = len(some_text)
-            if(count == 0):
-                print("Can't open", name, "or the file does not contain character")
-            else:
-                with open(copy_name, 'wb') as fc:
-                    fc.write(some_text.upper())
-                    print('Done')
+        with open(copy_name, 'w') as fc:
+                fc.write(some_text.upper())
+                print('Done')
     except FileNotFoundError:
         print('Error. File not found')
         
