cdbbf4a6e91e3469431f17498020c64582962b6f
the-twins <marina-budnikova8@rambler.ru>
2025-10-30 11:42:15 +0300

    Fixed some bugs

--- chapter_13/ex_05_randbin.py
+++ chapter_13/ex_05_randbin.py
@@ -5,6 +5,7 @@ if __name__ == '__main__':
     ARSIZE = 1000
     if(len(sys.argv) < 2):
         print('Invalid input on command line')
+        sys.exit(1)
     with open (sys.argv[1], "w") as f:
         for i in range(0, ARSIZE):
             numbers = 100.0 * i + 1.0 / (i + 1)
@@ -14,7 +15,7 @@ if __name__ == '__main__':
         print('Enter an index in the range 0 -', ARSIZE - 1)
         n = int(input())
         while(n >= 0 and n < ARSIZE):
-            for i in range(0, n):
+            for i in range(0, n + 1):
                 value = f.readline()
             print('The value there is', value)
             f.seek(0)
