c1683150f1617a51296e3c3e81084b5cc524de51
the-twins <marina-budnikova8@rambler.ru>
2025-10-30 10:51:12 +0300

    Fixed some bugs

--- chapter_13/ex_04_print_files.py
+++ chapter_13/ex_04_print_files.py
@@ -5,8 +5,12 @@ if __name__ == '__main__':
     numb = len(sys.argv)
     if(numb < 2):
         print('Invalid input on command line')
-    for i in range(1, numb):
-        with open(sys.argv[i], 'r') as f:
-            some_data = f.read()
-            print(some_data)
+    try:
+        for i in range(1, numb):
+            with open(sys.argv[i], 'r') as f:
+                some_data = f.read()
+                print(some_data)
+    except FileNotFoundError:
+        print('Error. File not found')
+        sys.exit(1)       
             
