8893c54881214f812a12ef08c82e656ca9b7e352
the-twins <marina-budnikova8@rambler.ru>
2025-11-07 13:28:43 +0300

    Fixed some bugs

--- chapter_13/ex_08_count_character_in_files.py
+++ chapter_13/ex_08_count_character_in_files.py
@@ -1,36 +1,31 @@
 import sys
 
 
-if __name__ == '__main__':
+def count(text: str, ch: str) -> int:
+    """Returnes count of the character"""
     count = 0
-    ch = sys.argv[1]
-    if(len(sys.argv) < 2):
+    for i in text:
+        if(i == ch):
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    if(len(sys.argv) <= 1):
         print('Invalid input on command line')
         sys.exit(1)
-    elif(len(sys.argv) == 2):
-        try:
-            print('Enter the file name:')
-            name = input()
-            with open(name, 'r') as f:
-                some_text = f.read()
-                for i in some_text:
-                    if(i == ch):
-                        count += 1
-            print('File', name, 'contains', count, 'characters', ch)
-        except FileNotFoundError:
-            print('Error. File', name,'not found')
-            sys.exit(1)
+    if(len(sys.argv) == 2):
+        print('Enter text to count the number of characters:')
+        text = input()
+        print('This text contains', count(text, sys.argv[1]), 'characters', sys.argv[1])
     else:
         numb = len(sys.argv)
         for i in range (2, numb):
             try:
                 with open(sys.argv[i], 'r') as f:
-                    some_text = f.read()
-                    for j in some_text:
-                        if(j == ch):
-                            count += 1
-                    print('File', sys.argv[i], 'contains', count, 'characters', ch)
-                    count = 0
+                    text = f.read()
+                    print('File', sys.argv[i], 'contains', count(text, sys.argv[1]), 'characters',
+                          sys.argv[1])
             except FileNotFoundError:
                 print('Error. File', sys.argv[i],'not found')           
                 
